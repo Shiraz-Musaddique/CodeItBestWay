6b30f71a49c8c7a14efe9cce0f3fee6a26edb132
Shiraz-Musaddique <shirazmusaddique@gmail.com>
2020-02-23 17:24:52 +0530

    Add files via upload

---
+++ CodeItBestWay/Python/Checking-number-is-3-digit-or-not.py
@@ -0,0 +1,6 @@
+# Python Code for checking the number is 3 digit or not
+i=int(input("Enter your number:"))
+if (i < 1000 and i > 99):
+  print(i, "is a 3 digit number ")
+else:
+  print(i, "is not a 3 digit number ")
