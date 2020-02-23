3d242645d8492f72fce3dae7019ea98a42e70ff7
Shiraz-Musaddique <shirazmusaddique@gmail.com>
2020-02-23 17:19:51 +0530

    Add files via upload

---
+++ CodeItBestWay/Python/BuzzFizz_Multiple_by_3and5_3_5.py
@@ -0,0 +1,15 @@
+#Famous BuzzFizz Code.. Written by Shiraz Musaddique
+i=int(input("Enter your number:"))
+if i%3==0 and i%5==0:
+        print("buzzfizz")
+
+elif i%3==0:
+        print("buzz")
+
+elif i%5==0:
+        print("fizz")
+
+else:
+        print(i)
+
+~                
---
+++ CodeItBestWay/Python/Odd-Even.py
@@ -0,0 +1,6 @@
+#Python code for checking Odd and Even number .....Written by Shiraz Musaddique
+i=int(input("Enter your number:"))
+if  i%2==0:
+  print(i, "is a even number ")
+else:
+  print(i, "is a odd number ")
